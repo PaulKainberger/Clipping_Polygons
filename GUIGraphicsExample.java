fc80b03e80cff4d369d4b6b81d0ea36e06610c84
PaulKainberger <49064272+PaulKainberger@users.noreply.github.com>
2019-05-19 22:46:12 +0200

    Update GUIGraphicsExample.java

--- GUIGraphicsExample.java
+++ GUIGraphicsExample.java
@@ -8,22 +8,23 @@ import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Panel extends JPanel
+public class GUIGraphicsExample extends JPanel
 {
-	public Panel() {
+	public GUIGraphicsExample()
+	{
 	}
 	private static final long serialVersionUID = 1L;
 	public void paintComponent(Graphics g)
 	{
-      super.paintComponent(g);
-      Polygon p = new Polygon();
-      for (int i = 0; i < 5; i++) p.addPoint((int) (
-         100 + 50 * Math.cos(i * 2 * Math.PI / 5)), (int) (
-         100 + 50 * Math.sin(i * 2 * Math.PI / 5)));
-      Polygon q = new Polygon();
-      for (int i = 0; i < 5; i++) q.addPoint((int) (
-         150 + 50 * Math.cos(i * 2 * Math.PI / 5)), (int) (
-         100 + 50 * Math.sin(i * 2 * Math.PI / 5)));
+	super.paintComponent(g);
+	Polygon p = new Polygon();
+	for (int i = 0; i < 5; i++) p.addPoint((int) (
+	100 + 50 * Math.cos(i * 2 * Math.PI / 5)), (int) (
+	100 + 50 * Math.sin(i * 2 * Math.PI / 5)));
+	Polygon q = new Polygon();
+	for (int i = 0; i < 5; i++) q.addPoint((int) (
+	150 + 50 * Math.cos(i * 2 * Math.PI / 5)), (int) (
+	100 + 50 * Math.sin(i * 2 * Math.PI / 5)));
       
       g.fillPolygon(p);
       g.setColor(Color.blue);
