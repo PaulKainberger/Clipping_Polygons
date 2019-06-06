12348f2f857fdd57d910fb792fe31fbb2f1864fe
PaulKainberger <paulchen.panther95@gmail.com>
2019-06-06 23:34:51 +0200

    First progress on algorithm.

--- src/Polygon/ClippingAlgorithm.java
+++ src/Polygon/ClippingAlgorithm.java
@@ -1,8 +1,10 @@
 package Polygon;
 
 import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 
@@ -166,8 +168,120 @@ public class ClippingAlgorithm {
 	 * @return Whether the clipping was successful.
 	 */
 	public boolean WeilerAtherton() {
-		//TODO: implement Weiler-Atherton algorithm
-		return false;
+		if(clippingPolygon == null || clippingPolygon.isSelfIntersecting()) {
+			return false;
+		}
+		
+		for(Polygon candidate : candidatePolygons) {
+			List<Polygon> result = clipWeilerAtherton(candidate);
+			if(result == null) {
+				return false;
+			} else {
+				resultPolygons.addAll(result);
+			}
+		}
+		return true;
+	}
+	
+	private List<Polygon> clipWeilerAtherton(Polygon candidate) {
+		// TODO method contains must be implemented (?)
+		/*if (clippingPolygon.contains(candidate)) {
+			return new Polygon(candidate);
+		}
+		if (candidate.contains(clippingPolygon)) {
+			return new Polygon();
+		}*/
+		
+		Set<Point2D.Double> intersectionPointsSet = null;
+		try {
+			intersectionPointsSet = candidate.intersect(clippingPolygon);
+		} catch (IntersectionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		List<Point2D.Double> intersectionPoints = new ArrayList<Point2D.Double>(intersectionPointsSet);
+		
+		Polygon candidateWithIntersections = new Polygon(candidate);
+		Polygon clippingWithIntersections = new Polygon(clippingPolygon);
+		List<Boolean> candidateIsIntersectionPoint = new ArrayList<Boolean>(Collections.nCopies(candidate.getNumberVertices(), false));
+		List<Boolean> clippingIsIntersectionPoint = new ArrayList<Boolean>(Collections.nCopies(clippingPolygon.getNumberVertices(), false));
+		
+		// Add intersection points to the polygons. Polygons do not change visually.
+		for (Point2D.Double intersectingPoint : intersectionPoints) {
+			// Add intersection points to candidate polygon.
+			boolean flag = true; // Indicates whether the intersection point lies between last and first vertex.
+			for (int i = 0; i < candidateWithIntersections.getNumberVertices() - 1; i++) {
+				if(Polygon.isBetween(candidateWithIntersections.getVertex(i), candidateWithIntersections.getVertex(i+1), intersectingPoint)) {
+					candidateWithIntersections.addVertex(intersectingPoint, i+1);
+					candidateIsIntersectionPoint.add(i+1, true);
+					flag = false;
+					break;
+				}
+			}
+			if(flag) {
+				candidateWithIntersections.addVertex(intersectingPoint);
+				candidateIsIntersectionPoint.add(true);
+			}
+			
+			// Add intersection points to clipping polygon.
+			flag = true; // Indicates whether the intersection point lies between last and first vertex.
+			for (int i = 0; i < clippingWithIntersections.getNumberVertices() - 1; i++) {
+				if(Polygon.isBetween(clippingWithIntersections.getVertex(i), clippingWithIntersections.getVertex(i+1), intersectingPoint)) {
+					clippingWithIntersections.addVertex(intersectingPoint, i+1);
+					clippingIsIntersectionPoint.add(i+1, true);
+					flag = false;
+					break;
+				}
+			}
+			if(flag) {
+				clippingWithIntersections.addVertex(intersectingPoint);
+				clippingIsIntersectionPoint.add(true);
+			}
+		}
+		
+		// Construct clipped polygons.
+		List<Polygon> clippedPols = new ArrayList<Polygon>();
+		while (! intersectionPoints.isEmpty()) {
+			// Construct new clipped polygon.
+			Polygon clipped = new Polygon();
+			Point2D.Double vertex = new Point2D.Double();
+			int i = candidateWithIntersections.vertices.indexOf(intersectionPoints.get(0)); // index for candidate polygon
+			// Start clipping.
+			do {
+				vertex = candidateWithIntersections.getVertex(i);
+				// vertex already in clipped??
+				// ...
+				clipped.addVertex(candidateWithIntersections.getVertex(i));
+				intersectionPoints.remove(candidateWithIntersections.getVertex(i));
+				i++;
+			}
+			while (candidateIsIntersectionPoint.get(i));
+			if(intersectionPoints.isEmpty()) {
+				break;
+			}
+			
+			// ...
+			
+			clippedPols.add(clipped);
+		}
+		
+		
+		
+		
+		
+		/*/int j = clippingWithIntersections.vertices.indexOf(intersectionPoints.get(0)); // index for clipping polygon
+		if(! clippingWithIntersections.contains(candidateWithIntersections.getVertex(i+1))) {
+			i++;
+			while (! candidateIsIntersectionPoint.get(i)) {
+				clipped.addVertex(clippingWithIntersections.getVertex(i));
+				i++;
+			}
+			intersectionPoints.remove(0);
+		}
+		clipped.addVertex(intersectionPoints.get(0));*/
+		
+		
+		return clippedPols;
 	}
 	
 	/**
